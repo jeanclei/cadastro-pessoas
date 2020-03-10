c38fdb472e0a63affa8c517a94ae368280cd2aa6
Jeanclei <borges0393@gmail.com>
2020-03-09 22:38:35 -0300

    Adicionado id_pessoafisica na migration documentos

--- src/migrations/20200309021224_documentos.js
+++ src/migrations/20200309021224_documentos.js
@@ -2,6 +2,7 @@
 exports.up = function (knex) {
     return knex.schema.createTable('documentos', table => {
         table.increments('id').primary()
+        table.integer('id_pessoafisica').references('pessoafisica.id').notNullable()
         table.integer('id_tipo_documento').references('tipo_documento.id').notNullable()
         table.string('numero')
         table.date('dtemiss')
